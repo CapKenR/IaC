a06830630b288bcfc0148b0b72efd4e11ee86bba
Ken Rider <ken.rider@capstonec.com>
2017-05-23 08:57:15 -0600

    Add Accredited Consultant training lab

---
+++ Training/Docker/Accredited-Consultant/manager-node-2.tf
@@ -0,0 +1,82 @@
+resource "azurerm_public_ip" "nm2-publicip" {
+  name                         = "manager-node-2-ip"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.resourcegroup.name}"
+  public_ip_address_allocation = "dynamic"
+  domain_name_label            = "cap-${var.user}-dac-manager-node-2"
+
+  tags {
+    environment = "${var.environment}"
+  }
+}
+
+resource "azurerm_network_interface" "nm2-networkinterface" {
+  name                = "manager-node-2-ni"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.resourcegroup.name}"
+  network_security_group_id = "${azurerm_network_security_group.nsg.id}"
+
+  ip_configuration {
+    name                          = "manager-node-2-ni-ip"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "dynamic"
+	public_ip_address_id          = "${azurerm_public_ip.nm2-publicip.id}"
+  }
+}
+
+resource "azurerm_virtual_machine" "nm2-vm" {
+  name                  = "manager-node-2"
+  location              = "${var.location}"
+  resource_group_name   = "${azurerm_resource_group.resourcegroup.name}"
+  network_interface_ids = ["${azurerm_network_interface.nm2-networkinterface.id}"]
+  vm_size               = "Standard_A0"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name          = "manager-node-2-os-disk"
+    vhd_uri       = "${azurerm_storage_account.storageaccount.primary_blob_endpoint}${azurerm_storage_container.storagecontainer.name}/manager-node-2.vhd"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "manager-node-2"
+    admin_username = "docker"
+    admin_password = "${var.password}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags {
+    environment = "${var.environment}"
+  }
+
+# Install Docker CE.
+  provisioner "remote-exec" {
+  
+    connection {
+	  type     = "ssh"
+	  host     = "${azurerm_public_ip.nm2-publicip.domain_name_label}.${var.location}.cloudapp.azure.com"
+	  user     = "docker"
+	  password = "${var.password}"
+	}
+	
+    inline = [
+      "sudo apt-get -y install apt-transport-https ca-certificates curl",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt-get update",
+      "sudo apt-get -y install docker-ce",
+	  "sudo groupadd docker",
+	  "sudo usermod -aG docker docker"
+    ]
+  }
+}
---
+++ Training/Docker/Accredited-Consultant/worker-node-0.tf
@@ -0,0 +1,82 @@
+resource "azurerm_public_ip" "wn0-publicip" {
+  name                         = "worker-node-0-ip"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.resourcegroup.name}"
+  public_ip_address_allocation = "dynamic"
+  domain_name_label            = "cap-${var.user}-dac-worker-node-0"
+
+  tags {
+    environment = "${var.environment}"
+  }
+}
+
+resource "azurerm_network_interface" "wn0-networkinterface" {
+  name                = "worker-node-0-ni"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.resourcegroup.name}"
+  network_security_group_id = "${azurerm_network_security_group.nsg.id}"
+
+  ip_configuration {
+    name                          = "worker-node-0-ni-ip"
+    subnet_id                     = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation = "dynamic"
+	public_ip_address_id          = "${azurerm_public_ip.wn0-publicip.id}"
+  }
+}
+
+resource "azurerm_virtual_machine" "wn0-vm" {
+  name                  = "worker-node-0"
+  location              = "${var.location}"
+  resource_group_name   = "${azurerm_resource_group.resourcegroup.name}"
+  network_interface_ids = ["${azurerm_network_interface.wn0-networkinterface.id}"]
+  vm_size               = "Standard_A0"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name          = "worker-node-0-os-disk"
+    vhd_uri       = "${azurerm_storage_account.storageaccount.primary_blob_endpoint}${azurerm_storage_container.storagecontainer.name}/worker-node-0.vhd"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "worker-node-0"
+    admin_username = "docker"
+    admin_password = "${var.password}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags {
+    environment = "${var.environment}"
+  }
+
+# Install Docker CE.
+  provisioner "remote-exec" {
+  
+    connection {
+	  type     = "ssh"
+	  host     = "${azurerm_public_ip.wn0-publicip.domain_name_label}.${var.location}.cloudapp.azure.com"
+	  user     = "docker"
+	  password = "${var.password}"
+	}
+	
+    inline = [
+      "sudo apt-get -y install apt-transport-https ca-certificates curl",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+      "sudo apt-get update",
+      "sudo apt-get -y install docker-ce",
+	  "sudo groupadd docker",
+	  "sudo usermod -aG docker docker"
+    ]
+  }
+}
