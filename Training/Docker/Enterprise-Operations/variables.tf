0344852a77c19807a7d706108527de85eddd0be2
Ken Rider <ken.rider@capstonec.com>
2017-05-22 21:56:49 -0600

    Add in variables
    Add in configuration variables and ignore Azure credentials in secret.tfvars.

---
+++ Training/Docker/Enterprise-Operations/variables.tf
@@ -0,0 +1,8 @@
+variable "user" {}
+variable "password" {}
+variable "location" { default = "westcentralus" }
+variable "environment" { default = "Training" }
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
